da69cf813043046aedb3de446bebcfee89c94b25
SNTSchool <tcpsholderch@gmail.com>
2025-12-16 08:39:09 +0700

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import Sidebar from './Sidebar';
+import React from 'react'
+import Sidebar from './Sidebar'
 
 export default function Layout({ children }) {
   return (
     <div className="min-h-screen bg-primary-50 flex">
       <Sidebar />
-      <main className="flex-1 p-6 md:ml-64">
+      <main className="flex-1 ml-64 p-6 md:p-8">
         {children}
       </main>
     </div>

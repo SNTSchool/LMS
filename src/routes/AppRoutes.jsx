7d2d9968b3e3b479bf21dadb3ac098a85c080af4
SNTSchool <tcpsholderch@gmail.com>
2025-12-21 14:22:13 +0700

    Update AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,29 +1,14 @@
-// src/routes/AppRoutes.jsx
 import { Routes, Route } from 'react-router-dom'
-import AppLayout from '../layouts/AppLayout'
-import ClassesPage from '../pages/Classes'
-import ClassDetail from '../pages/ClassDetail'
-import AttendanceScan from '../pages/AttendanceScan'
+import Classes from '../pages/Classes'
+import CreateClass from '../pages/CreateClass'
+import Classroom from '../pages/Classroom'
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route
-        path="/attendance/scan"
-        element={<AttendanceScan />}
-      />
-
-      <Route
-        path="/*"
-        element={
-          <AppLayout>
-            <Routes>
-              <Route path="classes" element={<ClassesPage />} />
-              <Route path="classes/:classId" element={<ClassDetail />} />
-            </Routes>
-          </AppLayout>
-        }
-      />
+      <Route path="/classes" element={<Classes />} />
+      <Route path="/classes/create" element={<CreateClass />} />
+      <Route path="/classes/:id" element={<Classroom />} />
     </Routes>
   )
 }

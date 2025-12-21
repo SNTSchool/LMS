9426eec2358082fa4a270b39e98edcb7ae327cb9
SNTSchool <tcpsholderch@gmail.com>
2025-12-21 15:40:00 +0700

    Update firebaseAdmin.js

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -1,15 +1,10 @@
 import admin from 'firebase-admin'
 
-const serviceAccount = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT)
-
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FB_PROJECT_ID,
-    clientEmail: process.env.FB_CLIENT_EMAIL,
-    privateKey: process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n')
-  })
+  credential: admin.credential.cert(
+    JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+  )
 })
 
-
-export const db = admin.firestore()
 export const auth = admin.auth()
+export const db = admin.firestore()

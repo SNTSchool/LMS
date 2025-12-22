5996c70d2ce0005e888e99523c84eacb11f64bab
SNTSchool <tcpsholderch@gmail.com>
2025-12-22 21:05:15 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,21 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Login from './pages/Login'
-import Classes from './pages/Classes'
-import Classroom from './pages/Classroom'
-import ProtectedRoute from './routes/ProtectedRoute'
-import { useAuth } from './routes/AuthProvider'
+<Routes>
+  <Route path="/login" element={<Login />} />
 
-function HomeRedirect() {
-  const { user, loading } = useAuth()
+  <Route
+    path="/classes"
+    element={
+      <ProtectedRoute>
+        <Classes />
+      </ProtectedRoute>
+    }
+  />
 
-  if (loading) {
-    return (
-      <div className="min-h-screen flex items-center justify-center">
-        Loading...
-      </div>
-    )
-  }
-
-  return user
-    ? <Navigate to="/classes" replace />
-    : <Navigate to="/login" replace />
-}
-
-export default function App() {
-  return (
-    <Routes>
-      {/* ⭐ MAIN ROOT */}
-      <Route path="/" element={<HomeRedirect />} />
-
-      <Route path="/login" element={<Login />} />
-
-      <Route
-        path="/classes"
-        element={
-          <ProtectedRoute>
-            <Classes />
-          </ProtectedRoute>
-        }
-      />
-
-      <Route
-        path="/classes/:id"
-        element={
-          <ProtectedRoute>
-            <Classroom />
-          </ProtectedRoute>
-        }
-      />
-
-      {/* (optional) 404 */}
-      <Route
-        path="*"
-        element={
-          <Navigate to="/" replace />
-        }
-      />
-    </Routes>
-  )
-}
+  <Route
+    path="/classes/:id"
+    element={
+      <ProtectedRoute>
+        <Classroom />
+      </ProtectedRoute>
+    }
+  />
+</Routes>

844367cce25d5116f8a330013506ca733f21a848
SNTSchool <tcpsholderch@gmail.com>
2025-12-22 19:32:39 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({
-      // ⭐ บังคับใช้ classic runtime (แก้ React is not defined)
-      jsxRuntime: 'classic',
+      jsxRuntime: 'automatic'
     })
-  ],
-
-  server: {
-    port: 5173,
-    open: true,
-  },
-
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-  },
-
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-
-  define: {
-    // ป้องกันบาง lib เรียก process.env แล้วพัง
-    'process.env': {},
-  }
+  ]
 })

c53e3bc409568dfd2ee86b329b3ad8da7ce8a482
SNTSchool <tcpsholderch@gmail.com>
2025-12-22 20:21:35 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react({
-      jsxRuntime: 'automatic'
-    })
-  ]
+  plugins: [react()],
+  resolve: {
+    dedupe: ['react', 'react-dom']
+  }
 })

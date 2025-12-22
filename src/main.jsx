ba2192839db5f65b0e577def721e86c881438ea4
SNTSchool <tcpsholderch@gmail.com>
2025-12-22 16:05:05 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
 import App from './App'
-import { AuthProvider } from './routes/AuthProvider'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 )

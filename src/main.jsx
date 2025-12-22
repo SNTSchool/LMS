e14f13925ca7e715e19afd9f0458874518ef9357
SNTSchool <tcpsholderch@gmail.com>
2025-12-22 08:21:01 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from 'react-dom/client'
 import App from './App'
-import './styles/index.css'
 import { AuthProvider } from './routes/AuthProvider'
+import { ErrorBoundary } from './ErrorBoundary'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <ErrorBoundary>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>
 )

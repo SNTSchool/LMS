fdbcfa03e8262ebea9504c0dea5ae58efedb4cab
SNTSchool <tcpsholderch@gmail.com>
2025-12-22 14:14:41 +0700

    Update AuthProvider.jsx

--- src/routes/AuthProvider.jsx
+++ src/routes/AuthProvider.jsx
@@ -1,7 +1,7 @@
 // src/routes/AuthProvider.jsx
 import { createContext, useContext, useEffect, useState } from 'react'
 import { onAuthStateChanged } from 'firebase/auth'
-import { auth } from 'firebase'
+import { auth } from '../firebaseConfig'
 import apiFetch from '../api/apiFetch'
 
 const AuthContext = createContext()

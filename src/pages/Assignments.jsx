fdff8512e762da1aeef5c3141093e43083e1f7c8
SNTSchool <tcpsholderch@gmail.com>
2025-12-22 19:43:01 +0700

    Create Assignments.jsx

---
+++ src/pages/Assignments.jsx
@@ -0,0 +1,8 @@
+export default function Assignments() {
+  return (
+    <div>
+      <h2 className="text-xl font-bold mb-4">Assignments</h2>
+      <p>หน้านี้ใช้สำหรับดูและส่งงาน</p>
+    </div>
+  )
+}
